b2d2dca04559e21b321c7efc4e20a961001ce931
Ana Ećim <147502958+anaecim@users.noreply.github.com>
2023-10-16 21:55:47 +0200

    Add files via upload

---
+++ Second_part_of_trying_homework_Ana.Rdm.R
@@ -0,0 +1,74 @@
+> library(escape)
+suppressPackageStartupMessages(library(Seurat))
+suppressPackageStartupMessages(library(ggplot2))
+
+colorblind_vector <- colorRampPalette(rev(c("#0D0887FF", "#47039FFF", 
+                                            "#7301A8FF", "#9C179EFF", "#BD3786FF", "#D8576BFF",
+                                            "#ED7953FF","#FA9E3BFF", "#FDC926FF", "#F0F921FF")))
+pbmc.data <- Read10X(data.dir = "C:/Users/Ana/Desktop/Emil/ORG_D45_filtered_feature_bc_matrix")
+
+pbmc <- CreateSeuratObject(counts = pbmc.data, project = "retina", min.cells = 3, min.features = 100)
+pbmc
+
+data <- data.frame(nCount_RNA = c(100, 200, 300), percent.mt = c(5, 10, 15))
+library(ggplot2)  
+ggplot(data, aes(x = nCount_RNA, y = percent.mt)) +
+  geom_point()
+
+pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+
+VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+
+plot1 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 + plot2
+
+pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 30)
+pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 4000 & percent.mt < 30)
+
+pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+
+top10 <- head(VariableFeatures(pbmc), 5)
+plot2 <- LabelPoints(plot = plot1, points = top5, repel = TRUE)
+plot1 + plot2
+
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc))
+
+VizDimLoadings(pbmc, dims = 1:2, reduction = "pca")
+DimPlot(pbmc, reduction = "pca")
+DimHeatmap(pbmc, dims = 1, cells = 100, balanced = TRUE)
+DimHeatmap(pbmc, dims = 1:10, cells = 100, balanced = TRUE)
+
+pbmc <- JackStraw(pbmc, num.replicate = 100)
+pbmc <- ScoreJackStraw(pbmc, dims = 1:20)
+JackStrawPlot(pbmc, dims = 1:17)
+ElbowPlot(pbmc)
+
+pbmc <- FindNeighbors(pbmc, dims = 1:17)
+pbmc <- FindClusters(pbmc, resolution = 0.5)
+
+head(Idents(pbmc), 5)
+
+pbmc <- RunUMAP(pbmc, dims = 1:17)
+DimPlot(pbmc, reduction = "umap")
+
+install.packages("msigdbr")
+library(msigdbr)
+msigdr <- msigdbr(species = "Homo sapiens", category = "H")
+
+data("escape.gene.sets", package="escape")
+gene.sets <- escape.gene.sets
+gene.sets <- list(Retinalcell_signature = c("PO4F2","CYP1B1")
+
+ES <- enrichIt(obj = seurat_ex, 
+gene.sets = gene.sets, 
+groups = 1000, cores = 4, 
+min.size = NULL)                 
+
+seurat_ex <- AddMetaData(pbmc, ES)
+seurat_ex@meta.data$active.idents <- seurat_ex@active.ident
+
+ES2 <- data.frame(seurat_ex[[]], Idents(seuart_ex))
+colnames(ES2)[ncol(ES2)] <- "cluster"
+
+And this is where I gave up, I do not know how to make ridge plots :(
